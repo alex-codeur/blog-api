a78146a9d6f938574d90727779c970818c65c6c2
esakimedia <apachecordovax@gmail.com>
2022-07-11 19:47:55 +0200

    setting up authentication

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-require('dotenv').config({ path: './config/.env' });
-
 //imports
 const userRoutes = require('./routes/user.routes');
+const { checkUser, requireAuth } = require('./middleware/auth.middleware');
 
 // database
 const connectDB = require('./config/connect');
+require('dotenv').config({ path: './config/.env' });
 
 // packages
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 
 // middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// jwt
+app.use('*', checkUser);
+app.use('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id);
+});
 
 //  routers
 app.use('/api/user', userRoutes);
--- models/user.model.js
+++ models/user.model.js
@@ -1,29 +1,33 @@
 const mongoose = require('mongoose');
-const { isEmail } = require('validator');
+const validator = require('validator');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
     {
         pseudo: {
             type: String,
-            required: true,
-            minLength: 3,
-            maxLength: 55,
+            required: [true, 'Please provide name'],
+            minlength: 3,
+            maxlength: 50,
             unique: true,
             trim: true
         },
         email: {
             type: String,
-            required: true,
-            validate: [isEmail],
+            unique: true,
+            required: [true, 'Please provide email'],
+            validate: {
+                validator: validator.isEmail,
+                message: 'Please provide valid email',
+            },
             lowercase: true,
             trim: true
         },
         password: {
             type: String,
-            required: true,
+            required: [true, 'Please provide password'],
             max: 1024,
-            minLength: 6
+            minlength: 6
         },
     },
     {
@@ -37,6 +41,20 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+
+        if(auth) {
+            return user;
+        }
+        throw Error('Incorrect password !');
+    }
+    throw Error('Incorrect email !');
+};
 
 
 const UserModel = mongoose.model('user', userSchema);
