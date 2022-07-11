6e10372e3c2fb077aaa52dd6e7e4028327e2d9fc
esakimedia <apachecordovax@gmail.com>
2022-07-11 13:35:15 +0200

    setting up user registration

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const express = require('express');
-
-const app = express();
-
-const connectDB = require('./config/connect');
 require('dotenv').config({ path: './config/.env' });
 
-const port = process.env.PORT || 5000;
+//imports
+const userRoutes = require('./routes/user.routes');
 
+// database
+const connectDB = require('./config/connect');
+
+// packages
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//  routers
+app.use('/api/user', userRoutes);
+
+// server
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
