8cee0d163aa34576a503ce8bd410c2a63e1c7f7f
esakimedia <apachecordovax@gmail.com>
2022-07-20 09:55:58 +0200

    posts CRUD

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,22 +2,60 @@ const PostModel = require('../models/post.model');
 const UserModel = require('../models/user.model');
 const ObjectID = require('mongoose').Types.ObjectId;
 
-module.exports.getAllPosts = (req, res) => {
+module.exports.getAllPosts = async (req, res) => {
+    try {
+        const posts = await PostModel.find({});
 
+        res.status(200).json(posts);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 };
 
-module.exports.getSinglePost = (req, res) => {
-    
+module.exports.getSinglePost = async (req, res) => {
+    try {
+        const post = await PostModel.findById(req.params.id);
+
+        res.status(200).json(post);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 };
 
-module.exports.createPost = (req, res) => {
-    
+module.exports.createPost = async (req, res) => {
+    const newPost = new PostModel(req.body);
+
+    try {
+        const savedPost = await newPost.save();
+
+        res.status(200).json(savedPost);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 };
 
-module.exports.updatePost = (req, res) => {
-    
+module.exports.updatePost = async (req, res) => {
+    try {
+        const updatedPost = await PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+
+        res.status(200).json(updatedPost);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 };
 
-module.exports.deletePost = (req, res) => {
-    
+module.exports.deletePost = async (req, res) => {
+    try {
+        await PostModel.deleteOne();
+
+        res.status(200).json("Post has been deleted...");
+    } catch(err) {
+        res.status(500).json(err);
+    }
 };
